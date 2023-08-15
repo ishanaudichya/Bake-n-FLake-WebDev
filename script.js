174189901f31e10b76a5bce4ddec4b90cb64bd1d
ishanaudichya <93155612+ishanaudichya@users.noreply.github.com>
2023-08-16 02:57:26 +0530

    Init

---
+++ script.js
@@ -0,0 +1,26 @@
+let menu = document.getElementById("nav");
+let open = document.getElementById("menu-btn");
+let close = document.getElementById("close");
+
+function openmenu() {
+  menu.style.left = "0";
+  open.style.display = "none";
+  close.style.display = "block";
+}
+function closemenu() {
+  menu.style.left = "-100%";
+  open.style.display = "block";
+  close.style.display = "none";
+}
+
+function change() {
+  var nav = document.getElementById("navbar");
+  var value = window.scrollY;
+  if (value > 80) {
+    nav.classList.add("nav-change");
+  } else {
+    nav.classList.remove("nav-change");
+  }
+}
+
+window.addEventListener("scroll", change);
